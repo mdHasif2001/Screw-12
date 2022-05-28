c2b57ab7f1394d6a2ab57a1f90f6da5e3d0d1169
mdHasif2001 <iamhasib727@gmail.com>
2022-05-29 00:05:45 +0600

    home page hasbeen touch

---
+++ src/Pages/Dashboard.js/Users.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import UserRow from '../Dashboard.js/UserRow';
+
+const Users = () => {
+  const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+    method: 'GET',
+    headers: {
+      authorization: `Beared ${localStorage.getItem('accessToken')}`
+    }
+  }).then(res => res.json()))
+
+  if (isLoading) {
+    return <Loading></Loading>
+  }
+
+  return (
+    <div>
+      <h2 className="text-3x">
+        All Users: {users.length}
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* <!-- head --> */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {
+              users.map(user => <UserRow
+                key={user._id}
+                user={user}
+                refetch={refetch}
+              >
+              </UserRow>)
+            }
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
